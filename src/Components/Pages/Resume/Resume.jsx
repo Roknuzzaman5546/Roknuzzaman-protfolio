5011f8f68b7f564062aa89e2431bd8a6d24a995f
Roknuzzaman5546 <roknuzzamansojib78@gmail.com>
2024-09-22 22:49:45 +0600

    exeperiance code added

--- src/Components/Pages/Resume/Resume.jsx
+++ src/Components/Pages/Resume/Resume.jsx
@@ -19,6 +19,19 @@ const Resume = () => {
             <h4 className=" text-right text-[#aaaa] text-xl mt-1">1 years of experience</h4>
             <div className=" grid md:grid-cols-10 mt-5 text-white gap-2">
                 <div className=" md:col-span-6 w-full">
+                    {/* Experience */}
+                    <h2 className="border-b-[4px] border-[#ff714a] text-2xl font-bold pb-1 inline-block mb-2 mt-5">Experience</h2>
+                    <div className=" flex">
+                        <div className=" md:w-[30%] w-[40%] text-right border-r border-gray-300 pr-2">
+                            <h2 className="font-bold">Shinygleam Software</h2>
+                            <p className=" text-gray-400 text-sm">2020,July-Present</p>
+                        </div>
+                        <div className=" md:w-[70%] w-[60%] md:pl-5 pl-3 mb-5">
+                            <h2 className=" font-bold">Jr Software Developer || Intern</h2>
+                            <p className=" text-sm text-gray-400 mt-2">As a Jr Software Developer when i join this company, From that time I am learning very well, Which helped me a lot to improve my skills।. In particular, I got a very good idea of ​​how a company should work on real projects. Although I had little prior experience in team projects, from here I am getting the best experience of working on team projects. But from here I am getting the best experience of working in team project. And the thing I learned the most is how to communicate and work together with all employees in a company. Which I think these things will be very useful in my future career
+                            </p>
+                        </div>
+                    </div>
                     <h2 className="border-b-[4px] border-[#ff714a] text-2xl font-bold pb-1 inline-block mb-2">Education</h2>
                     {/* Education part */}
                     <div>
@@ -53,11 +66,10 @@ const Resume = () => {
                             </div>
                         </div>
                     </div>
-                    {/* certificate */}
-                    <h2 className="border-b-[4px] border-[#ff714a] text-2xl font-bold pb-1 inline-block mb-2 mt-5">Certificate</h2>
+                    {/* certificate now comment when need then it uncomment */}
                     <div className=" mt-3">
                         {/* PH certificate */}
-                        <div className=" flex">
+                        {/* <div className=" flex">
                             <div className="md:w-[30%] w-[40%] text-right border-r border-gray-300 pr-2">
                                 <h2 className="font-bold">Programming hero</h2>
                                 <p className=" text-gray-400 text-sm">2023,June-20423,December</p>
@@ -67,15 +79,15 @@ const Resume = () => {
                                 <p className=" text-sm text-gray-400 mt-2">Thakurgaon polytechnic Institute is Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam praesentium enim similique deserunt optio possimus reprehenderit perferendis vitae! Repudiandae, temporibus? Distinctio, dolor? Animi praesentium id perferendis culpa reiciendis ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam praesentium enim similique deserunt optio possimus reprehenderit perferendis vitae! Repudiandae, temporibus? Distinctio, dolor? Animi praesentium id perferendis culpa reiciendis ut.
                                 </p>
                             </div>
-                        </div>
+                        </div> */}
                         {/* border section */}
-                        <div className=" flex">
+                        {/* <div className=" flex">
                             <div className=" md:w-[30%] w-[40%]"></div>
                             <div className="w-3 h-3 rounded-full border-2 border-[#ff714a] mr-2"></div>
                             <div className=" md:w-[70%] w-[60%]"></div>
-                        </div>
+                        </div> */}
                         {/* Great learning section python */}
-                        <div className=" flex">
+                        {/* <div className=" flex">
                             <div className="md:w-[30%] w-[40%] text-right border-r border-gray-300 pr-2">
                                 <h2 className="font-bold">Great learning</h2>
                                 <p className=" text-gray-400 text-sm">2024,February</p>
@@ -85,15 +97,15 @@ const Resume = () => {
                                 <p className=" text-sm text-gray-400 mt-2">Thakurgaon polytechnic Institute is Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam praesentium enim similique deserunt optio possimus reprehenderit perferendis vitae! Repudiandae, temporibus? Distinctio, dolor? Animi praesentium id perferendis culpa reiciendis ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam praesentium enim similique deserunt optio possimus reprehenderit perferendis vitae! Repudiandae, temporibus? Distinctio, dolor? Animi praesentium id perferendis culpa reiciendis ut.
                                 </p>
                             </div>
-                        </div>
+                        </div> */}
                         {/* border section */}
-                        <div className=" flex">
+                        {/* <div className=" flex">
                             <div className=" md:w-[30%] w-[40%]"></div>
                             <div className="w-3 h-3 rounded-full border-2 border-[#ff714a] mr-2"></div>
                             <div className=" md:w-[70%] w-[60%]"></div>
-                        </div>
+                        </div> */}
                         {/* Great learning php */}
-                        <div className=" flex">
+                        {/* <div className=" flex">
                             <div className="md:w-[30%] w-[40%] text-right border-r border-gray-300 pr-2">
                                 <h2 className="font-bold">Great Learning</h2>
                                 <p className=" text-gray-400 text-sm">2024,March</p>
@@ -103,12 +115,14 @@ const Resume = () => {
                                 <p className=" text-sm text-gray-400 mt-2">Thakurgaon polytechnic Institute is Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam praesentium enim similique deserunt optio possimus reprehenderit perferendis vitae! Repudiandae, temporibus? Distinctio, dolor? Animi praesentium id perferendis culpa reiciendis ut.Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium laboriosam praesentium enim similique deserunt optio possimus reprehenderit perferendis vitae! Repudiandae, temporibus? Distinctio, dolor? Animi praesentium id perferendis culpa reiciendis ut.
                                 </p>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                     {/* Download cv */}
-                    <button className="button-orange hover:bg-[#ff714a] mt-5 text-white font-serif hover:text-black text-xl font-bold">
-                        <a target="blank" href="https://drive.google.com/file/d/1F9yYcWU5sKh_ctndV8jxwZgq0tpuJTcu/view?usp=sharing">Download CV</a>
-                    </button>
+                    <a target="blank" href="https://drive.google.com/file/d/17ezT-XH6xKGCCYlfF-q5wh7LbukOq8Kz/view?usp=sharing">
+                        <button className="button-orange hover:bg-[#ff714a] mt-5 text-white font-serif hover:text-black text-xl font-bold">
+                            Download CV
+                        </button>
+                    </a>
                 </div>
                 {/* Coding skills */}
                 <div className=" md:col-span-4 ml-2">
@@ -217,7 +231,7 @@ const Resume = () => {
                     <div className=" mt-3">
                         <div className=" flex justify-between items-center mb-1">
                             <h2>Laravel with react</h2>
-                            <p className=" text-gray-400 text-sm  text-right">40%</p>
+                            <p className=" text-gray-400 text-sm  text-right">50%</p>
                         </div>
                         <div className=" w-full p-[2px] rounded-lg border border-[#ff714a] flex items-center">
                             <div className=" w-[50%] bg-[#ff714a] p-[2px] rounded-lg"></div>
